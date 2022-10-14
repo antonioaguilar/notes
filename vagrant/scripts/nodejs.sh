cea5e9431b2aec349bd0aeb75d20359b8fc5ed4a
Antonio Aguilar <antonioaguilar@users.noreply.gitlab.com>
2022-10-14 18:27:53 +0100

    updated config and keyboard bindings

--- vagrant/scripts/nodejs.sh
+++ vagrant/scripts/nodejs.sh
@@ -5,6 +5,4 @@ export NVM_VERSION="v0.39.1"
 # install NVM
 curl -s -k -o- https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh | bash
 
-nvm install --lts
-nvm use --lts
-nvm alias default $(node -v)
+nvm install --lts && nvm use --lts && nvm alias default $(node -v)
