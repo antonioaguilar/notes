ee435b7ebee0d928ec6b6a2cb4e9901347576d26
Antonio Aguilar <antonioaguilar@users.noreply.gitlab.com>
2024-06-20 16:33:28 +0100

    updated GCC development env

--- vagrant/scripts/gcc.sh
+++ vagrant/scripts/gcc.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get remove liblivemedia-dev
-apt-get install -y make cmake gcc g++ libssl-dev libpthread-stubs0-dev build-essential 
+apt-get install -y make cmake gcc g++ libnats-dev libssl-dev libpthread-stubs0-dev build-essential tree htop ncdu doxygen gdb git
