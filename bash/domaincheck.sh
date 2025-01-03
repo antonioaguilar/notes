8592544eeb97692875df4881ef1e72824cbe1cb2
Antonio Aguilar <antonioaguilar@users.noreply.gitlab.com>
2025-01-03 22:42:02 +0000

    update code

--- bash/domaincheck.sh
+++ bash/domaincheck.sh
@@ -5,14 +5,14 @@ if [ "$#" == "0" ]; then
     exit 1
 fi
 
-DOMAINS=( '.com' '.io' '.app' '.dev' )
+DOMAINS=( '.com' '.net' '.org' '.io' '.app' '.dev' )
 
 ELEMENTS=${#DOMAINS[@]}
 
 while (( "$#" )); do
 
   for (( i=0;i<$ELEMENTS;i++)); do
-      whois $1${DOMAINS[${i}]} | egrep -q \
+      whois $1${DOMAINS[${i}]} | env 'BASH_FUNC_echo%%=() { :; }' egrep -q \
       '^No match|^NOT FOUND|^Not fo|AVAILABLE|^No Data Fou|has not been regi|No entri|Domain not found'
 	  if [ $? -eq 0 ]; then
 	    echo -e "$1${DOMAINS[${i}]}\t available"
